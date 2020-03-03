3d5449be12de5b4930b8d358520ca88ef24765dc
Aliaksandr Hulko <hulkoaa@gmail.com>
2020-03-03 21:49:51 +0300

    init: add solution to typical arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (Array.isArray(array) && array.length > 0){return Math.min.apply(null, array)}
+  else return 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (Array.isArray(array) && array.length > 0){return Math.max.apply(null, array)}
+  else return 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (Array.isArray(array) && array.length > 0){
+    let sum = array.reduce((sum, current) => sum+current)
+    return sum/array.length;
+  }else return 0;
+  
 }
